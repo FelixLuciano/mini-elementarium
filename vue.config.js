62a1dd7a162e260b7521371d6d94fa94f526b32b
Luciano Felix Dias <luciano.felix.555@hotmail.com>
2019-05-16 06:33:14 -0300

    Fix icons URL

--- vue.config.js
+++ vue.config.js
@@ -30,16 +30,15 @@ module.exports = {
 
   pwa: {
     name: 'Mini Elementarium',
-    manifestPath: 'mini-elementarium/manifest.json',
     themeColor: '#FDD835',
     msTileColor: '#333333',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
     iconPaths: {
-      favicon32: 'mini-elementarium/img/icons/favicon-32x32.png',
-      favicon16: 'mini-elementarium/img/icons/favicon-16x16.png',
-      appleTouchIcon: 'mini-elementarium/img/icons/apple-touch-icon-152x152.png',
-      maskIcon: 'mini-elementarium/img/icons/safari-pinned-tab.svg',
-      msTileImage: 'mini-elementarium/img/icons/msapplication-icon-144x144.png'
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     }
   },
 
